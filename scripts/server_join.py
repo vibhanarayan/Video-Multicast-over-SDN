5c60a94eb2a388c692625483ca3e59d742075640
Sanyukta <sanyukta3@gatech.edu>
2016-11-21 08:24:00 -0800

    Adding reply from controller for server join message

--- scripts/server_join.py
+++ scripts/server_join.py
@@ -9,4 +9,6 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 print "Sending join message to controller"
 sock.sendto(video_id, (CONTROLLER_IP, SERVER_PORT))
 
+data, address = sock.recvfrom(2048)
+print data
 
--- video_multicast_app/shortestRouting.py
+++ video_multicast_app/shortestRouting.py
@@ -18,7 +18,8 @@ from MCIP_mapping import MCIPMap
 ETHERNET = ethernet.ethernet.__name__
 ARP = arp.arp.__name__
 IPV4 = ipv4.ipv4.__name__
-
+CONTROLLER_MAC = '08:00:27:a0:d2:9f'
+CONTROLLER_IP = '10.0.2.15'
 
 class Entry (object):
     def __init__ (self, port, mac):
@@ -60,8 +61,7 @@ class ShortestRouting(app_manager.RyuApp):
                             command=ofproto.OFPFC_ADD,idle_timeout=0, hard_timeout=0,
                             priority=priority,flags=ofproto.OFPFF_SEND_FLOW_REM,
                             instructions=inst)
-        print "adding flow", mod
-        print datapath.send_msg(mod)
+        datapath.send_msg(mod)
 
 
     @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
@@ -119,16 +119,17 @@ class ShortestRouting(app_manager.RyuApp):
         If the destination ip of the arp packet received is controller IP,
         then add a rule to forward all packets to controller, also send an arp reply to host.
         """
-        if arpPkt is not None and arpPkt.dst_ip == '10.0.2.15':
+        if arpPkt is not None and arpPkt.dst_ip == CONTROLLER_IP:
             out_port = ofproto.OFPP_CONTROLLER
             actions = [parser.OFPActionOutput(out_port)]
-            match = parser.OFPMatch(in_port=in_port, eth_dst=dst, arp_spa=ip_src, arp_tpa=ip_dst, eth_type=0x0800)
+            #match = parser.OFPMatch(in_port=in_port, eth_dst=dst, arp_spa=ip_src, arp_tpa=ip_dst, eth_type=0x0800)
+            match = parser.OFPMatch(in_port=in_port, eth_dst=dst, ipv4_src=ip_src, ipv4_dst=ip_dst, eth_type=0x0800)
             self.add_flow(datapath, 1, match, actions)
 
             arpreply = packet.Packet()
-            e = ethernet.ethernet(dst=eth.src, src='08:00:27:a0:d2:9f', ethertype=eth.ethertype)
-            a = arp.arp(opcode=arp.ARP_REPLY, src_mac='08:00:27:a0:d2:9f',
-                        src_ip='10.0.2.15', dst_mac=arpPkt.src_mac,
+            e = ethernet.ethernet(dst=eth.src, src=CONTROLLER_MAC, ethertype=eth.ethertype)
+            a = arp.arp(opcode=arp.ARP_REPLY, src_mac=CONTROLLER_MAC,
+                        src_ip=CONTROLLER_IP, dst_mac=arpPkt.src_mac,
                         dst_ip=arpPkt.src_ip)
             arpreply.add_protocol(e)
             arpreply.add_protocol(a)
@@ -139,11 +140,14 @@ class ShortestRouting(app_manager.RyuApp):
             print "ARP reply", arpreply
             out_port = in_port
             actions = [parser.OFPActionOutput(out_port)]
+            match = parser.OFPMatch(in_port=ofproto.OFPP_CONTROLLER, eth_dst=eth.src, ipv4_src=CONTROLLER_IP, ipv4_dst=arpPkt.src_ip, eth_type=0x0800)
+            self.add_flow(datapath, 1, match, actions)
+
             out = parser.OFPPacketOut(datapath=datapath, buffer_id=arpreply.buffer_id,
                                       in_port=ofproto.OFPP_CONTROLLER, actions=actions, data=data)
             datapath.send_msg(out)
 
-        elif udp_pkt and ip_pkt.dst == '10.0.2.15':
+        elif udp_pkt and ip_pkt.dst == CONTROLLER_IP:
             print "UDP ", udp_pkt
             if udp_pkt.dst_port == 8000:
                 video_id = pkt.protocols[-1]
@@ -157,18 +161,17 @@ class ShortestRouting(app_manager.RyuApp):
                 reply.add_protocol(ipv4.ipv4(version=4,
                                    proto=in_proto.IPPROTO_UDP,
                                    src=ip_pkt.dst,
-                                   dst=out_ip))
-                reply.add_protocol(udp.udp(src_port=8000))
+                                   dst=ip_pkt.src))
+                reply.add_protocol(udp.udp(dst_port=8000, src_port=8000))
                 reply.add_protocol(str(mcip))
                 reply.serialize()
-                print reply
+                print "Reply", reply
                 reply.buffer_id = ofproto.OFP_NO_BUFFER
                 data = reply.data
 
-                actions = [parser.OFPActionOutput(out_port)]
+                actions = [parser.OFPActionOutput(in_port)]
                 out = parser.OFPPacketOut(datapath=datapath, buffer_id=reply.buffer_id,
                                           in_port=ofproto.OFPP_CONTROLLER, actions=actions, data=data)
-                print "Out packet", out
                 datapath.send_msg(out)
 
             elif udp_pkt.dst_port == 8001:
@@ -209,10 +212,9 @@ class ShortestRouting(app_manager.RyuApp):
                 reply.buffer_id = ofproto.OFP_NO_BUFFER
                 data = reply.data
 
-                actions = [parser.OFPActionOutput(out_port)]
+                actions = [parser.OFPActionOutput(in_port)]
                 out = parser.OFPPacketOut(datapath=datapath, buffer_id=reply.buffer_id,
                                           in_port=ofproto.OFPP_CONTROLLER, actions=actions, data=data)
-                print "Out packet", out
                 datapath.send_msg(out)
 
         ###########################################
@@ -231,16 +233,17 @@ class ShortestRouting(app_manager.RyuApp):
             else:
                 out_port = ofproto.OFPP_FLOOD
 
-
             actions = [parser.OFPActionOutput(out_port)]
     
             if out_port != ofproto.OFPP_FLOOD:
                 #add flow basd on the IP address
                 if ARP in header_list:
-                    match = parser.OFPMatch(in_port=in_port, eth_dst=dst, arp_spa=ip_src, arp_tpa=ip_dst, eth_type=0x0800)
-                elif IPV4 in header_list:
+                    #match = parser.OFPMatch(in_port=in_port, eth_dst=dst, arp_spa=ip_src, arp_tpa=ip_dst, eth_type=0x0800)
                     match = parser.OFPMatch(in_port=in_port, eth_dst=dst, ipv4_src=ip_src, ipv4_dst=ip_dst, eth_type=0x0800)
-                self.add_flow(datapath, 1, match, actions)
+                    self.add_flow(datapath, 1, match, actions)
+                #elif IPV4 in header_list:
+                    #match = parser.OFPMatch(in_port=in_port, eth_dst=dst, ipv4_src=ip_src, ipv4_dst=ip_dst, eth_type=0x0800)
+                #self.add_flow(datapath, 1, match, actions)
 
             data = None
             if msg.buffer_id == ofproto.OFP_NO_BUFFER:
